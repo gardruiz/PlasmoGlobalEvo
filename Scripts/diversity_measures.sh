9f719c49f6c56140813873079a63f017a15e720e
Daniel Garcia-Ruiz <gard_ruiz@hotmail.com>
2024-08-14 15:14:56 +0800

    Removed unused files and added a project flowchart

--- Scripts/diversity_measures.sh
+++
@@ -1,9 +0,0 @@
-
-#Nucleotide diversiy pi
-vcftools --vcf input_file --window-pi 10000 --out output_file
-
-#Tajima's D
-vcftools --vcf input_file --TajimaD 10000 --out output_file
-
-#Fst
-vcftools --vcf all_samples.vcf --weir-fst-pop population1 --weir-fst-pop population2 --fst-window-size 10000 --out pop1_vs_pop2_FST_10kb
